ed9ef5e1a4a2cc54d9d47cd57adf28645e7747cb
perlancar (on pc-home) <perlancar@gmail.com>
2019-10-19 07:58:19 +0700

    Release v0.001

--- lib/eval.pm
+++ lib/eval.pm
@@ -5,10 +5,11 @@ package eval;
 
 sub import {
     my $class = shift;
-    for (@_) {
-        eval $_;
-        die if $@;
-    }
+    my $code = join ",", @_;
+    return unless length $code;
+    print "eval.pm: Eval'ing: $code\n" if $ENV{DEBUG};
+    eval $code;
+    die if $@;
 }
 
 1;
@@ -20,7 +21,8 @@ Use in one-liner or in PERL5OPT:
 
  % perl -Meval='$Carp::MaxArgLen = 1000' script.pl
 
- % PERL5OPT='-Meval=$Carp::MaxArgLen = 1000' script.pl
+ % DEBUG=1 PERL5OPT='-Meval=$Carp::MaxArgLen=1000' script.pl
+ eval.pm: Eval'ing: $Carp::MaxArgLen=1000
 
 
 =head1 DESCRIPTION
@@ -45,3 +47,33 @@ For example, you put your code in F<lib/Foo.pm> and run your script using:
  % perl -e'$Carp::MaxArgLen = 1000; do "script.pl"'
 
 =back
+
+Some caveats:
+
+=over
+
+=item * No whitespace in code in PERL5OPT
+
+Space is used to separate perl options in PERL5OPT, so you can't do:
+
+ % PERL5OPT='-Meval=$Carp::MaxArgLen = 1000' script.pl
+
+but you can do:
+
+ % PERL5OPT='-Meval=$Carp::MaxArgLen=1000' script.pl
+
+To help you see what code is being eval'ed, set C<DEBUG> to true:
+
+ % PERL5OPT='-Meval=$Carp::MaxArgLen=1000' script.pl
+ eval.pm: Eval'ing: $Carp::MaxArgLen=1000
+
+=back
+
+
+=head1 ENVIRONMENT
+
+=head2 DEBUG
+
+Bool. If set to true, will show what code is being eval'ed.
+
+=cut
